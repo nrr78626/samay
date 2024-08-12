47469d87fa857273a418068d4397fc7d8910a0a1
Narendra Rajbhar <narendrarajbhar547.com>
2024-08-12 20:10:41 +0530

    next-auth custom providers

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,20 @@
+"use client"
+import { useSession, signIn, signOut } from "next-auth/react"
+
+export default function Component() {
+  const { data: session } = useSession()
+  if (session) {
+    return (
+      <>
+        Signed in as {session.user.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    )
+  }
+  return (
+    <>
+      Not signed in <br />
+      <button className="bg-violet-500 px-2 py-1 text-slate-50 font-semibold" onClick={() => signIn()}>Sign in</button>
+    </>
+  )
+}
